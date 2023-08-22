17a06298cd4330b5927d85850a05f6d089e5d7fa
jcw1031 <jcw001031@gmail.com>
2023-08-23 01:03:41 +0900

    [GOORM] 챌린지

--- src/main/java/woopaca/Main.java
+++ src/main/java/woopaca/Main.java
@@ -1,5 +1,63 @@
 package woopaca;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
+    private static final int[] ROW_D = new int[]{0, 0, 1, 1, -1, -1, 1, -1};
+    private static final int[] COL_D = new int[]{1, -1, 1, -1, 1, -1, 0, 0};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] gameBoard = new int[n][n];
+        int[][] flags = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                gameBoard[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (gameBoard[i][j] == 1) {
+                    updateFlagsValue(gameBoard, flags, i, j);
+                }
+            }
+        }
+
+        System.out.println(getFlagsCount(flags, k));
+    }
+
+    private static void updateFlagsValue(int[][] gameBoard, int[][] flags, int row, int col) {
+        for (int i = 0; i < ROW_D.length; i++) {
+            int newRow = row + ROW_D[i];
+            int newCol = col + COL_D[i];
+            if (newRow < 0 || newRow >= flags.length || newCol < 0 || newCol >= flags.length ||
+                    gameBoard[newRow][newCol] == 1) {
+                continue;
+            }
+
+            flags[newRow][newCol]++;
+        }
+    }
+
+    private static int getFlagsCount(int[][] flags, int k) {
+        int count = 0;
+        for (int i = 0; i < flags.length; i++) {
+            for (int j = 0; j < flags.length; j++) {
+                if (flags[i][j] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/woopaca/programmers/lv2/거리두기확인하기Test.java
+++ src/test/java/woopaca/programmers/lv2/거리두기확인하기Test.java
@@ -2,14 +2,35 @@ package woopaca.programmers.lv2;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class 거리두기확인하기Test {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+    }
+
     private final 거리두기확인하기 거리두기확인하기 = new 거리두기확인하기();
 
     @Test
-    void test() {
+    void test() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        writer.write(1);
+        writer.flush();
+
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int n = Integer.parseInt(tokenizer.nextToken());
         int[] answer = 거리두기확인하기.solution(new String[][]{
                 {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
                 {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
@@ -18,6 +39,28 @@ class 거리두기확인하기Test {
                 {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}
         });
 
+        EnableTime[] enableTimes = new EnableTime[n];
+
+        Arrays.sort(enableTimes, Comparator.comparingInt(EnableTime::getStart));
         assertArrayEquals(new int[]{1, 0, 1, 1, 1}, answer);
+    }
+}
+
+class EnableTime {
+
+    private int start;
+    private int end;
+
+    public EnableTime(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
     }
 }
