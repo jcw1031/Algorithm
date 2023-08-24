6e8ff6b47e66ce440f964e03c544fdd93f0b6885
jcw1031 <jcw001031@gmail.com>
2023-08-24 11:46:27 +0900

    [GOORM] 챌린지 9일차

--- src/main/java/woopaca/Main.java
+++ src/main/java/woopaca/Main.java
@@ -2,12 +2,16 @@ package woopaca;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int[] ROW_D = new int[]{0, 0, 1, 1, -1, -1, 1, -1};
-    private static final int[] COL_D = new int[]{1, -1, 1, -1, 1, -1, 0, 0};
+    private static final int[] ROW_D = new int[]{0, 0, 1, -1};
+    private static final int[] COL_D = new int[]{1, -1, 0, 0};
+
+    private static char[][] groundStatus;
+    private static int[][] bombValues;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,49 +19,62 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] gameBoard = new int[n][n];
-        int[][] flags = new int[n][n];
+        groundStatus = new char[n][n];
+        bombValues = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                gameBoard[i][j] = Integer.parseInt(st.nextToken());
+                groundStatus[i][j] = st.nextToken().charAt(0);
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (gameBoard[i][j] == 1) {
-                    updateFlagsValue(gameBoard, flags, i, j);
-                }
-            }
+        for (int i = 0; i < k; i++) {
+            int[] coordinates = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(value -> Integer.parseInt(value) - 1)
+                    .toArray();
+            updateBombValue(coordinates[0], coordinates[1]);
+
+            updateSurroundBombValue(coordinates);
         }
 
-        System.out.println(getFlagsCount(flags, k));
+        System.out.println(getMaxBombValue());
     }
 
-    private static void updateFlagsValue(int[][] gameBoard, int[][] flags, int row, int col) {
-        for (int i = 0; i < ROW_D.length; i++) {
-            int newRow = row + ROW_D[i];
-            int newCol = col + COL_D[i];
-            if (newRow < 0 || newRow >= flags.length || newCol < 0 || newCol >= flags.length ||
-                    gameBoard[newRow][newCol] == 1) {
+    private static void updateSurroundBombValue(int[] coordinates) {
+        for (int j = 0; j < ROW_D.length; j++) {
+            int newRow = coordinates[0] + ROW_D[j];
+            int newCol = coordinates[1] + COL_D[j];
+            if (newRow < 0 || newCol < 0 || newRow >= groundStatus.length || newCol >= groundStatus.length) {
                 continue;
             }
 
-            flags[newRow][newCol]++;
+            updateBombValue(newRow, newCol);
         }
     }
 
-    private static int getFlagsCount(int[][] flags, int k) {
-        int count = 0;
-        for (int i = 0; i < flags.length; i++) {
-            for (int j = 0; j < flags.length; j++) {
-                if (flags[i][j] == k) {
-                    count++;
+    private static void updateBombValue(int row, int col) {
+        switch (groundStatus[row][col]) {
+            case '0': {
+                bombValues[row][col] += 1;
+                break;
+            }
+            case '@': {
+                bombValues[row][col] += 2;
+                break;
+            }
+        }
+    }
+
+    private static int getMaxBombValue() {
+        int max = 0;
+        for (int[] bombValuesRow : bombValues) {
+            for (int bombValue : bombValuesRow) {
+                if (max < bombValue) {
+                    max = bombValue;
                 }
             }
         }
-        return count;
+        return max;
     }
 }
