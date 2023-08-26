92e8a361f45c6cffe36561b57daa44473c3de670
jcw1031 <jcw001031@gmail.com>
2023-08-26 10:44:13 +0900

    [GOORM] 챌린지 10일차

--- src/main/java/woopaca/Main.java
+++ src/main/java/woopaca/Main.java
@@ -1,80 +1,92 @@
 package woopaca;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int[] ROW_D = new int[]{0, 0, 1, -1};
-    private static final int[] COL_D = new int[]{1, -1, 0, 0};
-
-    private static char[][] groundStatus;
-    private static int[][] bombValues;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        groundStatus = new char[n][n];
-        bombValues = new int[n][n];
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] goormPosition = {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1};
+        boolean[][] goormVisited = new boolean[n][n];
 
+        st = new StringTokenizer(br.readLine());
+        int[] playerPosition = {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1};
+        boolean[][] playerVisited = new boolean[n][n];
+
+        String[][] board = new String[n][n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                groundStatus[i][j] = st.nextToken().charAt(0);
+                board[i][j] = st.nextToken();
             }
         }
 
-        for (int i = 0; i < k; i++) {
-            int[] coordinates = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(value -> Integer.parseInt(value) - 1)
-                    .toArray();
-            updateBombValue(coordinates[0], coordinates[1]);
+        int goormScore = move(goormPosition, goormVisited, 1, board, n);
+        int playerScore = move(playerPosition, playerVisited, 1, board, n);
 
-            updateSurroundBombValue(coordinates);
+        if (goormScore > playerScore) {
+            System.out.println("goorm " + goormScore);
+        } else if (goormScore < playerScore) {
+            System.out.println("player " + playerScore);
         }
 
-        System.out.println(getMaxBombValue());
+        br.close();
     }
 
-    private static void updateSurroundBombValue(int[] coordinates) {
-        for (int j = 0; j < ROW_D.length; j++) {
-            int newRow = coordinates[0] + ROW_D[j];
-            int newCol = coordinates[1] + COL_D[j];
-            if (newRow < 0 || newCol < 0 || newRow >= groundStatus.length || newCol >= groundStatus.length) {
-                continue;
-            }
 
-            updateBombValue(newRow, newCol);
+    public static int set_Pos(int a, int n) {
+        if (a == -1) {
+            return n - 1;
         }
+        if (a == n) {
+            return 0;
+        }
+        return a;
     }
 
-    private static void updateBombValue(int row, int col) {
-        switch (groundStatus[row][col]) {
-            case '0': {
-                bombValues[row][col] += 1;
-                break;
-            }
-            case '@': {
-                bombValues[row][col] += 2;
-                break;
-            }
+    private static final HashMap<String, int[]> directions = new HashMap<>() {
+        {
+            put("U", new int[]{-1, 0});
+            put("D", new int[]{1, 0});
+            put("L", new int[]{0, -1});
+            put("R", new int[]{0, 1});
         }
-    }
+    };
 
-    private static int getMaxBombValue() {
-        int max = 0;
-        for (int[] bombValuesRow : bombValues) {
-            for (int bombValue : bombValuesRow) {
-                if (max < bombValue) {
-                    max = bombValue;
+    public static int move(int[] pos, boolean[][] visited, int score, String[][] board, int n) {
+        int x = pos[0];
+        int y = pos[1];
+        visited[x][y] = true;
+        boolean flag = true;
+
+        while (flag) {
+            String command = board[x][y];
+            int distance = Integer.parseInt(command.substring(0, command.length() - 1));
+            String direction = command.substring(command.length() - 1);
+
+            for (int i = 0; i < distance; i++) {
+                x += directions.get(direction)[0];
+                y += directions.get(direction)[1];
+                x = set_Pos(x, n);
+                y = set_Pos(y, n);
+
+                if (!visited[x][y]) {
+                    visited[x][y] = true;
+                    score += 1;
+                } else {
+                    flag = false;
+                    break;
                 }
             }
         }
-        return max;
+        return score;
     }
 }
