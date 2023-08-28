73431e3be73d66194c881b408da6b38f9eede8a4
jcw1031 <jcw001031@gmail.com>
2023-08-28 12:46:24 +0900

    [GOORM] 챌린지 11일차

--- src/main/java/woopaca/Main.java
+++ src/main/java/woopaca/Main.java
@@ -3,90 +3,43 @@ package woopaca;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
+    private static final int MAX_VALUE = (int) (Math.pow(10, 6) + 1);
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        int[] goormPosition = {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1};
-        boolean[][] goormVisited = new boolean[n][n];
+        int[] painValue = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        st = new StringTokenizer(br.readLine());
-        int[] playerPosition = {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1};
-        boolean[][] playerVisited = new boolean[n][n];
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = MAX_VALUE;
+        }
 
-        String[][] board = new String[n][n];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                board[i][j] = st.nextToken();
+        for (int i = 0; i < 2; i++) {
+            if (painValue[i] > n) {
+                continue;
             }
-        }
-
-        int goormScore = move(goormPosition, goormVisited, 1, board, n);
-        int playerScore = move(playerPosition, playerVisited, 1, board, n);
-
-        if (goormScore > playerScore) {
-            System.out.println("goorm " + goormScore);
-        } else if (goormScore < playerScore) {
-            System.out.println("player " + playerScore);
-        }
-
-        br.close();
-    }
-
-
-    public static int set_Pos(int a, int n) {
-        if (a == -1) {
-            return n - 1;
-        }
-        if (a == n) {
-            return 0;
-        }
-        return a;
-    }
-
-    private static final HashMap<String, int[]> directions = new HashMap<>() {
-        {
-            put("U", new int[]{-1, 0});
-            put("D", new int[]{1, 0});
-            put("L", new int[]{0, -1});
-            put("R", new int[]{0, 1});
-        }
-    };
-
-    public static int move(int[] pos, boolean[][] visited, int score, String[][] board, int n) {
-        int x = pos[0];
-        int y = pos[1];
-        visited[x][y] = true;
-        boolean flag = true;
-
-        while (flag) {
-            String command = board[x][y];
-            int distance = Integer.parseInt(command.substring(0, command.length() - 1));
-            String direction = command.substring(command.length() - 1);
-
-            for (int i = 0; i < distance; i++) {
-                x += directions.get(direction)[0];
-                y += directions.get(direction)[1];
-                x = set_Pos(x, n);
-                y = set_Pos(y, n);
-
-                if (!visited[x][y]) {
-                    visited[x][y] = true;
-                    score += 1;
-                } else {
-                    flag = false;
-                    break;
+            dp[painValue[i]] = 1;
+            for (int j = 1; j <= n; j++) {
+                if (j < painValue[i]) {
+                    continue;
                 }
+                int oldValue = dp[j - painValue[i]];
+                if (oldValue == MAX_VALUE) {
+                    continue;
+                }
+
+                dp[j] = Integer.min(oldValue + 1, dp[j]);
             }
         }
-        return score;
+
+        System.out.println(dp[n] == MAX_VALUE ? -1 : dp[n]);
     }
 }
