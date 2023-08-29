d9b57b2607fe702b0fdb8e8f616cab3153b8ab3b
jcw1031 <jcw001031@gmail.com>
2023-08-29 18:43:26 +0900

    [GOORM] 챌린지 12일차

--- src/main/java/woopaca/Main.java
+++ src/main/java/woopaca/Main.java
@@ -3,43 +3,57 @@ package woopaca;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-    private static final int MAX_VALUE = (int) (Math.pow(10, 6) + 1);
+    private static final int[][] DIRECTION = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] painValue = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        int[] dp = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i] = MAX_VALUE;
+        char[][] m = new char[n][n];
+        boolean[][] isVisited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            m[i] = br.readLine().replaceAll(" ", "").toCharArray();
         }
 
-        for (int i = 0; i < 2; i++) {
-            if (painValue[i] > n) {
-                continue;
-            }
-            dp[painValue[i]] = 1;
-            for (int j = 1; j <= n; j++) {
-                if (j < painValue[i]) {
-                    continue;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (m[i][j] == '1' && !isVisited[i][j]) {
+                    count++;
+                    bfs(n, m, isVisited, i, j);
                 }
-                int oldValue = dp[j - painValue[i]];
-                if (oldValue == MAX_VALUE) {
-                    continue;
-                }
-
-                dp[j] = Integer.min(oldValue + 1, dp[j]);
             }
         }
 
-        System.out.println(dp[n] == MAX_VALUE ? -1 : dp[n]);
+        System.out.println(count);
+    }
+
+    private static void bfs(int n, char[][] m, boolean[][] isVisited, int startRow, int startCol) {
+        Queue<int[]> queue = new LinkedList<>();
+        isVisited[startRow][startCol] = true;
+        queue.offer(new int[]{startRow, startCol});
+
+        while (!queue.isEmpty()) {
+            int[] coordinates = queue.poll();
+            int row = coordinates[0];
+            int col = coordinates[1];
+
+            for (int[] ints : DIRECTION) {
+                int nextRow = ints[0] + row;
+                int nextCol = ints[1] + col;
+                if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n ||
+                        isVisited[nextRow][nextCol] || m[nextRow][nextCol] != '1') {
+                    continue;
+                }
+
+                isVisited[nextRow][nextCol] = true;
+                queue.add(new int[]{nextRow, nextCol});
+            }
+        }
     }
 }
