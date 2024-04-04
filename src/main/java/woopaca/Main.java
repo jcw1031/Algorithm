1fc3bb6e184edba0fc51ef79d6d836107c7aba87
jcw1031 <jcw001031@gmail.com>
2024-04-04 15:56:35 +0900

    baekjoon 14940 solved

--- src/main/java/woopaca/Main.java
+++ src/main/java/woopaca/Main.java
@@ -3,66 +3,100 @@ package woopaca;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final StringBuilder result = new StringBuilder();
+    private static final int TARGET_POINT = 2;
+    private static final int IMMOVABLE_POINT = 0;
 
-    private static int[] list;
-    private static boolean[] isUsed;
-    private static int[] selectedNumbers;
     private static int n;
     private static int m;
+    private static int targetX;
+    private static int targetY;
+    private static int[][] map;
+    private static int[][] distanceMap;
+    private static boolean[][] isVisited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
         n = Integer.parseInt(tokenizer.nextToken());
         m = Integer.parseInt(tokenizer.nextToken());
 
-        list = new int[n];
-        isUsed = new boolean[n];
-        selectedNumbers = new int[m];
+        map = new int[n][m];
+        distanceMap = new int[n][m];
+        isVisited = new boolean[n][m];
 
-        tokenizer = new StringTokenizer(reader.readLine());
         for (int i = 0; i < n; i++) {
-            int number = Integer.parseInt(tokenizer.nextToken());
-            list[i] = number;
+            tokenizer = new StringTokenizer(reader.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(tokenizer.nextToken());
+                distanceMap[i][j] = -1;
+                if (map[i][j] == TARGET_POINT) {
+                    targetY = i;
+                    targetX = j;
+                }
+            }
         }
-        Arrays.sort(list);
 
-        peakNumbers(0);
+        bfs(new Point(targetX, targetY, 0));
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result.append(map[i][j] == IMMOVABLE_POINT ? 0 : distanceMap[i][j])
+                        .append(" ");
+            }
+            result.append(System.lineSeparator());
+        }
         System.out.println(result);
     }
 
-    private static void peakNumbers(int depth) {
-        if (depth == m) {
-            printNumbers(selectedNumbers);
-            return;
-        }
+    private static void bfs(Point targetPoint) {
+        final Queue<Point> queue = new LinkedList<>();
+        queue.offer(targetPoint);
+        isVisited[targetPoint.y][targetPoint.x] = true;
 
-        int previousNumber = -1;
-        for (int i = 0; i < n; i++) {
-            if (!isUsed[i] && previousNumber != list[i]) {
-                selectedNumbers[depth] = list[i];
-                isUsed[i] = true;
-                previousNumber = list[i];
-                peakNumbers(depth + 1);
-                isUsed[i] = false;
-            }
+        while (!queue.isEmpty()) {
+            final Point point = queue.poll();
+            distanceMap[point.y][point.x] = point.distance;
+
+            addPoint(queue, new Point(point.x + 1, point.y, point.distance + 1));
+            addPoint(queue, new Point(point.x - 1, point.y, point.distance + 1));
+            addPoint(queue, new Point(point.x, point.y + 1, point.distance + 1));
+            addPoint(queue, new Point(point.x, point.y - 1, point.distance + 1));
         }
     }
 
-    private static void printNumbers(int[] numbers) {
-        /*result.append(Arrays.stream(numbers)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining(" ")));*/
-        for (int number : numbers) {
-            result.append(number)
-                    .append(" ");
+    private static void addPoint(Queue<Point> queue, Point point) {
+        if (point.x < 0 || point.y < 0 || point.y >= n || point.x >= m
+                || isVisited[point.y][point.x]) {
+            return;
         }
-        result.append(System.lineSeparator());
+        if (map[point.y][point.x] == IMMOVABLE_POINT) {
+            distanceMap[point.y][point.x] = 0;
+            isVisited[point.y][point.x] = true;
+            return;
+        }
+
+        queue.offer(point);
+        isVisited[point.y][point.x] = true;
+    }
+
+    static class Point {
+
+        final int x;
+        final int y;
+        final int distance;
+
+        public Point(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
     }
 }
