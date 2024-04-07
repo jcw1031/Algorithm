35b6057e8978f0b03a570ecb27ad45cf9aab7a66
jcw1031 <jcw001031@gmail.com>
2024-04-07 11:21:07 +0900

    baekjoon 1914 solved

--- src/main/java/woopaca/beakjoon/Main.java
+++ src/main/java/woopaca/beakjoon/Main.java
@@ -3,96 +3,47 @@ package woopaca.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int HOUSE = 1;
-    private static final int CHICKEN = 2;
-
-    private static int n;
-    private static int m;
-    private static List<Point> chickens;
-    private static List<Point> houses;
-    private static Point[] selectedChickens;
-    private static int minimumDistance = Integer.MAX_VALUE;
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        chickens = new ArrayList<>();
-        houses = new ArrayList<>();
 
-        n = Integer.parseInt(tokenizer.nextToken());
-        m = Integer.parseInt(tokenizer.nextToken());
-        for (int i = 0; i < n; i++) {
-            tokenizer = new StringTokenizer(reader.readLine());
-            for (int j = 0; j < n; j++) {
-                int input = Integer.parseInt(tokenizer.nextToken());
-                if (input == CHICKEN) {
-                    chickens.add(new Point(i, j));
-                }
-                if (input == HOUSE) {
-                    houses.add(new Point(i, j));
-                }
-            }
-        }
+        int n = Integer.parseInt(reader.readLine());
+        List<int[]> processes = new ArrayList<>();
 
-        selectedChickens = new Point[m];
-        combination(0, 0);
-
-        System.out.println(minimumDistance);
-    }
-
-    private static void combination(int depth, int startIndex) {
-        if (depth == m) {
-            int chickenDistance = calculateChickenDistance();
-            if (minimumDistance > chickenDistance) {
-                minimumDistance = chickenDistance;
-            }
+        if (n > 20) {
+            System.out.println(BigInteger.valueOf(2).pow(n).subtract(BigInteger.ONE));
             return;
         }
 
-        for (int i = startIndex; i < chickens.size(); i++) {
-            selectedChickens[depth] = chickens.get(i);
-            combination(depth + 1, i + 1);
+        hanoi(n, 1, 3, processes);
+        int size = processes.size();
+        StringBuilder result = new StringBuilder();
+        result.append(size)
+                .append("\n");
+        for (int i = 0; i < size; i++) {
+            int[] process = processes.get(i);
+            result.append(process[0])
+                    .append(" ")
+                    .append(process[1])
+                    .append("\n");
         }
+        System.out.println(result);
     }
 
-    private static int calculateChickenDistance() {
-        Map<Point, Integer> distances = new HashMap<>();
-        for (Point chicken : selectedChickens) {
-            for (Point house : houses) {
-                int distance = chicken.getDistanceBetween(house);
-                if (distance < distances.getOrDefault(house, Integer.MAX_VALUE)) {
-                    distances.put(house, distance);
-                }
-            }
+    private static void hanoi(int n, int from, int to, List<int[]> processes) {
+        if (n == 1) {
+            processes.add(new int[]{from, to});
+            return;
         }
 
-        int chickenDistance = 0;
-        for (Integer value : distances.values()) {
-            chickenDistance += value;
-        }
-        return chickenDistance;
-    }
-
-    static class Point {
-
-        final int row;
-        final int col;
-
-        public Point(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        public int getDistanceBetween(Point other) {
-            return Math.abs(this.row - other.row) + Math.abs(this.col - other.col);
-        }
+        int empty = 6 - from - to;
+        hanoi(n - 1, from, empty, processes);
+        hanoi(1, from, to, processes);
+        hanoi(n - 1, empty, to, processes);
     }
 }
