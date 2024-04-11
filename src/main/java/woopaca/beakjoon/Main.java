225e739bcd466fb231bb232377150f570075388d
jcw1031 <jcw001031@gmail.com>
2024-04-11 16:44:57 +0900

    baekjoon 11403 solved

--- src/main/java/woopaca/beakjoon/Main.java
+++ src/main/java/woopaca/beakjoon/Main.java
@@ -3,47 +3,46 @@ package woopaca.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    private static int[][] map;
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer;
 
         int n = Integer.parseInt(reader.readLine());
-        List<int[]> processes = new ArrayList<>();
+        map = new int[n][n];
 
-        if (n > 20) {
-            System.out.println(BigInteger.valueOf(2).pow(n).subtract(BigInteger.ONE));
-            return;
+        for (int i = 0; i < n; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(tokenizer.nextToken());
+            }
         }
 
-        hanoi(n, 1, 3, processes);
-        int size = processes.size();
+        // j -> i -> k
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (map[j][i] == 1 && map[i][k] == 1) {
+                        map[j][k] = 1;
+                    }
+                }
+            }
+        }
+
         StringBuilder result = new StringBuilder();
-        result.append(size)
-                .append("\n");
-        for (int i = 0; i < size; i++) {
-            int[] process = processes.get(i);
-            result.append(process[0])
-                    .append(" ")
-                    .append(process[1])
-                    .append("\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result.append(map[i][j])
+                        .append(" ");
+            }
+            result.append(System.lineSeparator());
         }
+
         System.out.println(result);
-    }
-
-    private static void hanoi(int n, int from, int to, List<int[]> processes) {
-        if (n == 1) {
-            processes.add(new int[]{from, to});
-            return;
-        }
-
-        int empty = 6 - from - to;
-        hanoi(n - 1, from, empty, processes);
-        hanoi(1, from, to, processes);
-        hanoi(n - 1, empty, to, processes);
     }
 }
