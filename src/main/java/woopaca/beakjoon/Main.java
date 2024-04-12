a2de3750b1822e065d2992cd4ab2445fc0257b4c
jcw1031 <jcw001031@gmail.com>
2024-04-12 11:35:46 +0900

    baekjoon 1967 solved

--- src/main/java/woopaca/beakjoon/Main.java
+++ src/main/java/woopaca/beakjoon/Main.java
@@ -3,46 +3,68 @@ package woopaca.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[][] map;
+    private static List<Node>[] graph;
+    private static boolean[] isVisited;
+    private static int maxDistance = -1;
+    private static int maxDistanceNode;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer;
 
         int n = Integer.parseInt(reader.readLine());
-        map = new int[n][n];
+        graph = new ArrayList[n + 1];
+        isVisited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             tokenizer = new StringTokenizer(reader.readLine());
-            for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(tokenizer.nextToken());
+            int parentNode = Integer.parseInt(tokenizer.nextToken());
+            int childNode = Integer.parseInt(tokenizer.nextToken());
+            int weight = Integer.parseInt(tokenizer.nextToken());
+
+            graph[parentNode].add(new Node(childNode, weight));
+            graph[childNode].add(new Node(parentNode, weight));
+        }
+
+        findFarthestNode(1, 0);
+        isVisited = new boolean[n + 1];
+        findFarthestNode(maxDistanceNode, 0);
+        System.out.println(maxDistance);
+    }
+
+    private static void findFarthestNode(int currentNode, int distance) {
+        isVisited[currentNode] = true;
+        List<Node> nodes = graph[currentNode];
+
+        for (Node node : nodes) {
+            if (!isVisited[node.number]) {
+                findFarthestNode(node.number, distance + node.weight);
             }
         }
 
-        // j -> i -> k
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (map[j][i] == 1 && map[i][k] == 1) {
-                        map[j][k] = 1;
-                    }
-                }
-            }
+        if (maxDistance < distance) {
+            maxDistance = distance;
+            maxDistanceNode = currentNode;
         }
+    }
 
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                result.append(map[i][j])
-                        .append(" ");
-            }
-            result.append(System.lineSeparator());
+    static class Node {
+
+        int number;
+        int weight;
+
+        public Node(int number, int weight) {
+            this.number = number;
+            this.weight = weight;
         }
-
-        System.out.println(result);
     }
 }
